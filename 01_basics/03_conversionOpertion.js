65465571375b7b995fe001d04fb6b8eb0552db59
Vickyy05 <vickywork1005@gmail.com>
2024-10-30 14:20:31 +0000

    Conversation of Datatypes

---
+++ 01_basics/03_conversionOpertion.js
@@ -0,0 +1,41 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Vivek"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isloggedIn = 1 
+// let isloggedIn = ""
+let isloggedIn = "Vivek"
+
+
+let BooleanIsloggedIn = Boolean(isloggedIn)
+// console.log(BooleanIsloggedIn); 
+
+
+// 1 => true; 0 => false 
+// "" => false 
+// "Vivek" => true
+
+
+
+let someNumber = 33 
+
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
