8ec07343393b1314267bfd0be52d0e8b306e0cc6
Vickyy05 <vickywork1005@gmail.com>
2024-10-30 13:55:44 +0000

    datatypes comit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //  treat all JS code as a newer version
+
+// alert( 3+3)   // we are using nodejs, not browser
+
+console.log(3 
+    + 3) // code readability should be High  
+
+console.log("Vivek")
+ 
+
+
+let name = "Vivek"
+let age = 20
+let isloggedIn = true 
+let state;
+
+// number => 2 to power 53
+// biginit    // for larger numbers 
+// string => "" 
+// boolean => true/false 
+// null => standalone value 
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined);  // undefined 
+console.log(typeof null);      // object 
